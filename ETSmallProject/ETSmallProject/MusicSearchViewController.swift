03f94e1ee527a86b438b5671f0173db4b92c8310
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-10 15:46:17 +0800

    Create custom text field and add it to music search page: - Create custom UITextView subclass "SearchTextField" - Setup text field constraint and hierarchy in music search page

--- ETSmallProject/ETSmallProject/MusicSearchViewController.swift
+++ ETSmallProject/ETSmallProject/MusicSearchViewController.swift
@@ -2,10 +2,12 @@ import UIKit
 
 class MusicSearchViewController: UIViewController {
 	private let searchTitleLabel = UILabel()
+	private let searchTextField = SearchTextField()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		view.backgroundColor = .white
+
 		setupViewHierarchy()
 		setupViews()
 		setupConstraints()
@@ -15,14 +17,22 @@ class MusicSearchViewController: UIViewController {
 extension MusicSearchViewController {
 	private func setupViewHierarchy() {
 		view.addSubview(searchTitleLabel)
+		view.addSubview(searchTextField)
 	}
 	
 	private func setupViews() {
 		searchTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-		
-		view.backgroundColor = .white
+		searchTextField.translatesAutoresizingMaskIntoConstraints = false
+
 		searchTitleLabel.text = "Search Music"
 		searchTitleLabel.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+		
+		let placeHolderAttributes: [NSAttributedString.Key: Any] = [
+			.font: UIFont.systemFont(ofSize: 14.0, weight: .medium),
+			.foregroundColor: UIColor.black.withAlphaComponent(0.5)
+		]
+		
+		searchTextField.attributedPlaceholder = NSAttributedString(string: "Enter keyword here", attributes: placeHolderAttributes)
 	}
 	
 	private func setupConstraints() {
@@ -30,6 +40,13 @@ extension MusicSearchViewController {
 			searchTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
 			searchTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
 			searchTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12)
+		])
+		
+		NSLayoutConstraint.activate([
+			searchTextField.topAnchor.constraint(equalTo: searchTitleLabel.bottomAnchor, constant: 4),
+			searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+			searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+			searchTextField.heightAnchor.constraint(equalToConstant: 36)
 		])
 	}
 }
