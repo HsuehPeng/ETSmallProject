3380909d05da70b1d1da8c309268244e309bb4f8
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-10 15:28:22 +0800

    Add search title label to music search page: - Add "searchTitleLabel" - Setup label's constraints, view hierarchy and font

--- ETSmallProject/ETSmallProject/MusicSearchViewController.swift
+++ ETSmallProject/ETSmallProject/MusicSearchViewController.swift
@@ -1,19 +1,36 @@
-//
-//  ViewController.swift
-//  ETSmallProject
-//
-//  Created by Hsueh Peng Tseng on 2024/7/10.
-//
-
 import UIKit
 
 class MusicSearchViewController: UIViewController {
+	private let searchTitleLabel = UILabel()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		setupViewHierarchy()
+		setupViews()
+		setupConstraints()
 	}
+}
 
-
+extension MusicSearchViewController {
+	private func setupViewHierarchy() {
+		view.addSubview(searchTitleLabel)
+	}
+	
+	private func setupViews() {
+		searchTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+		
+		view.backgroundColor = .white
+		searchTitleLabel.text = "Search Music"
+		searchTitleLabel.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+	}
+	
+	private func setupConstraints() {
+		NSLayoutConstraint.activate([
+			searchTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			searchTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+			searchTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12)
+		])
+	}
 }
 
