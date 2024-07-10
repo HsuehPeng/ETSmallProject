49f8c1b4d58008fd83763f45672a99e57a0c68f4
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-10 16:25:52 +0800

    Add search button to music search page - Create "searchButton" and setup it's constraints and view hierarchy

--- ETSmallProject/ETSmallProject/MusicSearchViewController.swift
+++ ETSmallProject/ETSmallProject/MusicSearchViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class MusicSearchViewController: UIViewController {
 	private let searchTitleLabel = UILabel()
 	private let searchTextField = SearchTextField()
+	private let searchButton = UIButton()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -18,11 +19,13 @@ extension MusicSearchViewController {
 	private func setupViewHierarchy() {
 		view.addSubview(searchTitleLabel)
 		view.addSubview(searchTextField)
+		view.addSubview(searchButton)
 	}
 	
 	private func setupViews() {
 		searchTitleLabel.translatesAutoresizingMaskIntoConstraints = false
 		searchTextField.translatesAutoresizingMaskIntoConstraints = false
+		searchButton.translatesAutoresizingMaskIntoConstraints = false
 
 		searchTitleLabel.text = "Search Music"
 		searchTitleLabel.font = UIFont.systemFont(ofSize: 14, weight: .bold)
@@ -33,6 +36,12 @@ extension MusicSearchViewController {
 		]
 		
 		searchTextField.attributedPlaceholder = NSAttributedString(string: "Enter keyword here", attributes: placeHolderAttributes)
+		
+		searchButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+		searchButton.titleLabel?.textColor = UIColor.white
+		searchButton.setTitle("Search", for: .normal)
+		searchButton.layer.cornerRadius = 8
+		searchButton.backgroundColor = UIColor.black
 	}
 	
 	private func setupConstraints() {
@@ -47,6 +56,13 @@ extension MusicSearchViewController {
 			searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
 			searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
 			searchTextField.heightAnchor.constraint(equalToConstant: 36)
+		])
+		
+		NSLayoutConstraint.activate([
+			searchButton.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 12),
+			searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+			searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+			searchButton.heightAnchor.constraint(equalToConstant: 42)
 		])
 	}
 }
