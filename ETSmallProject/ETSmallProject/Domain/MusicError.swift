ac1d0926d26caf82cabca52d8452764d27a7bd7a
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-13 19:47:28 +0800

    Add invalid data error as the decoding error

--- ETSmallProject/ETSmallProject/Domain/MusicError.swift
+++ ETSmallProject/ETSmallProject/Domain/MusicError.swift
@@ -3,6 +3,7 @@ import Foundation
 enum MusicError: Error {
 	case search
 	case unknown
+	case invalidData
 	
 	var errorModel: ErrorModel {
 		switch self {
@@ -10,6 +11,8 @@ enum MusicError: Error {
 			return ErrorModel(title: "Search fail", description: "Fail to get the music result")
 		case .unknown:
 			return ErrorModel(title: "Unknown Error", description: "Unknown Error")
+		case .invalidData:
+			return ErrorModel(title: "Invalid data", description: "Invalid dat")
 		}
 	}
 }
