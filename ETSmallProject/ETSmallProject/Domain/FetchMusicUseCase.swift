0047dc0f29d853d83167555f38cee61276755138
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-13 15:00:08 +0800

    Create domain class "FetchMusicUseCase" which is responsible for fetching music list

---
+++ ETSmallProject/ETSmallProject/Domain/FetchMusicUseCase.swift
@@ -0,0 +1,20 @@
+import RxSwift
+
+protocol FetchMusicUseCaseProtocol {
+	typealias FetchResult = Result<[Music], Error>
+	
+	func execute(searchTerm: String) -> Observable<FetchResult>
+}
+
+final class FetchMusicUseCase: FetchMusicUseCaseProtocol {
+	func execute(searchTerm: String) -> Observable<FetchResult> {
+		let musics: [Music] = [
+			.init(id: "1", trackName: "Track name1", trackTimeMillis: 7154613, artworkUrl100: "", longDescription: "longDescription"),
+			.init(id: "1", trackName: "Track name1", trackTimeMillis: 7154613, artworkUrl100: "", longDescription: "longDescription"),
+			.init(id: "1", trackName: "Track name1", trackTimeMillis: 7154613, artworkUrl100: "", longDescription: "longDescription"),
+			.init(id: "1", trackName: "Track name1", trackTimeMillis: 7154613, artworkUrl100: "", longDescription: "longDescription")
+		]
+
+		return Observable.just(.success(musics)).delay(.seconds(2), scheduler: ConcurrentDispatchQueueScheduler(qos: .userInitiated))
+	}
+}
