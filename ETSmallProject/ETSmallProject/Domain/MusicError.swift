9db653a07b2c7f890e69bb120b03e1910edb045d
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-13 15:23:45 +0800

    Add unknown error case to domain error

--- ETSmallProject/ETSmallProject/Domain/MusicError.swift
+++ ETSmallProject/ETSmallProject/Domain/MusicError.swift
@@ -2,4 +2,14 @@ import Foundation
 
 enum MusicError: Error {
 	case search
+	case unknown
+	
+	var errorModel: ErrorModel {
+		switch self {
+		case .search:
+			return ErrorModel(title: "Search fail", description: "Fail to get the music result")
+		case .unknown:
+			return ErrorModel(title: "Unknown Error", description: "Unknown Error")
+		}
+	}
 }
