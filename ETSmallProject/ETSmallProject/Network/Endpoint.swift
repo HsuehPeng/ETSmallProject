e39c9345d2b22af5e6b75b59bcb75353476554e6
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-13 16:32:40 +0800

    Create "Endpoint" for constructing "URLRequest"

---
+++ ETSmallProject/ETSmallProject/Network/Endpoint.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+protocol Endpoint {
+	var baseURL: URL { get }
+	var path: String { get }
+	var method: HTTPMethod { get }
+	var headers: [String: String]? { get }
+	var body: Data? { get }
+}
+
+extension Endpoint {
+	func makeRequest() -> URLRequest? {
+		guard var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: false) else {
+			return nil
+		}
+		components.path = path
+		
+		guard let url = components.url else {
+			return nil
+		}
+		
+		var request = URLRequest(url: url)
+		request.httpMethod = method.rawValue
+		request.allHTTPHeaderFields = headers
+		request.httpBody = body
+		return request
+	}
+}
