94269bb897c22928a1688c465b4bcec368fc58b7
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-14 23:38:39 +0800

    Use "musicManagerUseCase" to control music playing behavior

--- ETSmallProject/ETSmallProject/Presentation/MusicSearchViewModel.swift
+++ ETSmallProject/ETSmallProject/Presentation/MusicSearchViewModel.swift
@@ -5,6 +5,7 @@ import RxSwift
 
 final class MusicSearchViewModel {
 	private let fetchMusicUseCase: FetchMusicUseCaseProtocol
+	private let musicManagerUseCase: MusicManagerUseCaseProtocol
 	private let disposebag = DisposeBag()
 	
 	private let musicCellVMsRelay = BehaviorRelay<[MusicCollectionViewCellViewModel]>(value: [])
@@ -12,25 +13,39 @@ final class MusicSearchViewModel {
 	private let isLoadingRelay = BehaviorRelay<Bool>(value: false)
 	private let playingIndexRelay = BehaviorRelay<Int?>(value: nil)
 	
-	init(fetchMusicUseCase: FetchMusicUseCaseProtocol) {
+	init(
+		fetchMusicUseCase: FetchMusicUseCaseProtocol,
+		musicManagerUseCase: MusicManagerUseCaseProtocol
+	) {
 		self.fetchMusicUseCase = fetchMusicUseCase
+		self.musicManagerUseCase = musicManagerUseCase
 	}
 
 	func transform(input: Input) -> Output {
 		input.didTapCellItemSignal.emit(with: self, onNext: { owner, indexPath in
-			guard let currentIndex = owner.playingIndexRelay.value else {
-				owner.musicCellVMsRelay.value[indexPath.item].togglePlayState()
-				owner.playingIndexRelay.accept(indexPath.item)
+			let selectedIndex = indexPath.item
+			let selectedMusicItemVM = owner.musicCellVMsRelay.value[selectedIndex]
+			
+			guard let previousSelectedIndex = owner.playingIndexRelay.value else {
+				selectedMusicItemVM.togglePlayState()
+				owner.playingIndexRelay.accept(selectedIndex)
+				owner.musicManagerUseCase.start(from: URL(string: selectedMusicItemVM.music.previewUrl)!)
 				return
 			}
 			
-			if indexPath.item == currentIndex {
-				owner.musicCellVMsRelay.value[currentIndex].togglePlayState()
-				owner.playingIndexRelay.accept(currentIndex)
+			if selectedIndex == previousSelectedIndex {
+				selectedMusicItemVM.togglePlayState()
+				owner.playingIndexRelay.accept(previousSelectedIndex)
+				if selectedMusicItemVM.playState == .playing {
+					owner.musicManagerUseCase.resume()
+				} else if selectedMusicItemVM.playState == .pause {
+					owner.musicManagerUseCase.pause()
+				}
 			} else {
-				owner.musicCellVMsRelay.value[currentIndex].removePlayState()
-				owner.musicCellVMsRelay.value[indexPath.item].togglePlayState()
-				owner.playingIndexRelay.accept(indexPath.item)
+				owner.musicCellVMsRelay.value[previousSelectedIndex].removePlayState()
+				selectedMusicItemVM.togglePlayState()
+				owner.playingIndexRelay.accept(selectedIndex)
+				owner.musicManagerUseCase.start(from: URL(string: selectedMusicItemVM.music.previewUrl)!)
 			}
 		}).disposed(by: disposebag)
 		
