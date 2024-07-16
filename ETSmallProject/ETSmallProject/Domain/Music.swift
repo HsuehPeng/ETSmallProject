b64aa71709321998350a058891f386bc8853a89a
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-16 20:31:07 +0800

    Make music initializer public

--- ETSmallProject/ETSmallProject/Domain/Music.swift
+++ ETSmallProject/ETSmallProject/Domain/Music.swift
@@ -6,4 +6,18 @@ public struct Music: Equatable {
 	public let artworkUrl100: String?
 	public let longDescription: String?
 	public let previewUrl: String
+	
+	public init(
+		trackName: String,
+		trackTimeMillis: Int,
+		artworkUrl100: String?,
+		longDescription: String?,
+		previewUrl: String
+	) {
+		self.trackName = trackName
+		self.trackTimeMillis = trackTimeMillis
+		self.artworkUrl100 = artworkUrl100
+		self.longDescription = longDescription
+		self.previewUrl = previewUrl
+	}
 }
