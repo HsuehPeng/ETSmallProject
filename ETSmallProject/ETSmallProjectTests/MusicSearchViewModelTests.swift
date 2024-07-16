570f7ee967572235c9c4c80c5df3db703c51e553
Hsueh Peng Tseng <qaz5899971@gmail.com>
2024-07-17 02:15:54 +0800

    MusicSearchViewModelTests - Return no items when failed to search music

--- ETSmallProject/ETSmallProjectTests/MusicSearchViewModelTests.swift
+++ ETSmallProject/ETSmallProjectTests/MusicSearchViewModelTests.swift
@@ -61,6 +61,28 @@ final class MusicSearchViewModelTests: XCTestCase {
 		XCTAssertEqual(results, [expectedSections])
 	}
 	
+	func test_failedToSearchMusic_returnNoMusicItems() {
+		let searchTermObservable: TestableObservable<String> = scheduler.createHotObservable([.next(0, "searchTerm")])
+		let searchButtonTapObservable: TestableObservable<Void> = scheduler.createHotObservable([.next(100, ())])
+		let cellItemTapObservable: TestableObservable<IndexPath> = scheduler.createHotObservable([])
+		mockFetchMusicUseCase.fetchResult = .just(.failure(.unknown))
+		let resultObserver = scheduler.createObserver([MusicSearchViewModel.SectionModel].self)
+
+		let output = viewModel.transform(
+			input: .init(
+				searchTermDriver: searchTermObservable.asDriver(onErrorJustReturn: ""),
+				searchButtonTapSignal: searchButtonTapObservable.asSignal(onErrorJustReturn: ()),
+				didTapCellItemSignal: cellItemTapObservable.asSignal(onErrorJustReturn: IndexPath(item: 0, section: 0))
+			)
+		)
+		output.dataSourceDriver.skip(2).asObservable().subscribe(resultObserver).disposed(by: disposeBag)
+		
+		scheduler.start()
+		
+		let results = resultObserver.events.compactMap { $0.value.element }
+		XCTAssertEqual(results, [])
+	}
+	
 	class MockFetchMusicUseCase: FetchMusicUseCaseProtocol {
 		var fetchResult: Observable<Result<[Music], MusicError>>?
 		
